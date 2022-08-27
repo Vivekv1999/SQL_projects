95dee00fbfaac719a0be5ee0abf4db110fee70f2
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-08-27 15:05:18 +0530

    Updated

--- sqlll-11.sql
+++ sqlll-11.sql
@@ -2,3 +2,4 @@ USE tops;
 
 CREATE TABLE general(FIRST_name VARCHAR(40),LAST_name VARCHAR(40),address VARCHAR(40),city VARCHAR(40),age INT);
 INSERT INTO general VALUES("mickey","mouse","123 fantasy way","anaheim",73),("bat","man","321 kavern ave","gotham",54);
+ 
